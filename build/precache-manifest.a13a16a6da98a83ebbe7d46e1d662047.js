b4e8e380051b74bd025c1a281ef5aae098404c8d
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-10-05 19:06:27 -0600

    run this serve build command for checking the build

---
+++ build/precache-manifest.a13a16a6da98a83ebbe7d46e1d662047.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f68e3b29f04b8b02f0a2d39603403914",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f497804762149e052e76",
+    "url": "/static/css/2.17e61147.chunk.css"
+  },
+  {
+    "revision": "26e82b56f41c1e3d928d",
+    "url": "/static/css/main.15c4832a.chunk.css"
+  },
+  {
+    "revision": "f497804762149e052e76",
+    "url": "/static/js/2.66dd18de.chunk.js"
+  },
+  {
+    "revision": "c91cbb924d2e6dd2f7d23f894d2489e8",
+    "url": "/static/js/2.66dd18de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e82b56f41c1e3d928d",
+    "url": "/static/js/main.e2ec5965.chunk.js"
+  },
+  {
+    "revision": "243b8f0ab90e65c7feeb",
+    "url": "/static/js/runtime-main.89e9d1db.js"
+  }
+]);
